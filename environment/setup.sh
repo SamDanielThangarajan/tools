b0e07a655cedb6fb45d46a90f406fffd00f6da43
edansam <sam.a.daniel@ericsson.com>
2016-06-07 10:22:18 +0200

    Introduced TOOLS variable

--- environment/setup.sh
+++ environment/setup.sh
@@ -1,19 +1,15 @@
 [[ -z "$REPO_BASE" ]] && echo "Need to set variable REPO_BASE " && exit 1
+[[ -z "$TOOLS" ]] && echo "Need to set variable TOOLS " && exit 1
 
 BASEPATH=$(dirname -- "${BASH_SOURCE[0]}")
 SCRIPTPATH=$(cd -P "$BASEPATH" && pwd -P)
 
 
-if [ -f ~/tools/environment/sam_gitcomplete ]; then
-    source ~/tools/environment/sam_gitcomplete
-fi
-
-
 GIT_PS1_SHOWDIRTYSTATE=true
 
-export NODES_CONFIG=~/tools/environment/nodes.cfg
+#export NODES_CONFIG=${TOOLS}/environment/nodes.cfg
 
-source ~/tools/environment/sam_alias
+source ${TOOLS}/environment/sam_alias
 
 # Some VI exports to marry VIM with TMUX
 # Two giant robots colide :)
