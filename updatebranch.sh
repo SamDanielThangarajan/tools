525ffc422d63934f87860e3dd470ccd77e575394
Sam <sam.a.daniel@ericsson.com>
2017-09-28 09:35:45 +0200

    vi mode in tmux + updatebranch

--- updatebranch.sh
+++ updatebranch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -d ${CLONE_ROOT} ]; then
-    #branch=`cat ${CLONE_ROOT}/.git/HEAD | cut -d '/' -f 3`
-    branch=`cat ${CLONE_ROOT}/.git/HEAD | sed 's#ref: refs/heads/##g' | sed 's#feature/##g'` 
-    export GIT_BRANCH=${branch}
+
+if [[ -f ${CLONE_ROOT}/.git/HEAD ]]
+then
+   branch=`cat ${CLONE_ROOT}/.git/HEAD | sed 's#ref: refs/heads/##g' | sed 's#feature/##g'`
+   export GIT_BRANCH=${branch}
 else
-    export GIT_BRANCH="Nil"
+   export GIT_BRANCH="Nil"
 fi
