e9942769610cae6d6a22bbe8f6ea4180222a93d5
Sam <sam.a.daniel@ericsson.com>
2018-01-12 14:07:25 +0100

    merge_directories (WIP) + open git changed files

--- organize-pics.sh
+++ organize-pics.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+#NOT COMPLETE
+
 alias_file="$TOOLS/environment/alias"
 number_of_dirs_created=0
 number_of_files_moved=0
+dest_dir=""
+field=1
 declare -A month_converter=( \
    ["01"]="jan" \
    ["02"]="feb" \
@@ -25,12 +29,47 @@ cmd_to_list_files_alone="ls -l | grep ^- | awk -F' ' '{print \$9}'"
 shopt -s expand_aliases
 source $TOOLS/environment/alias
 
+################################################
+## Arguments
+###
+while getopts ":d:p:f:" opt; do
+   case $opt in
+      d)
+         dest_dir=$OPTARG
+         ;;
+      f)
+      field=$OPTARG
+         ;;
+      \?)
+         echo "Invalid option: -$OPTARG" >&2
+         exit 1
+         ;;
+      :)
+         echo "Option -$OPTARG requires an argument." >&2
+         exit 1
+   esac
+done
+
+[[ ${dest_dir} = "" ]] \
+   && usage \
+   && exit 1
+
+################################################
+## Main
+###
+
+cmd="ls -1 | cut -d '_' -f $field | sort | uniq | egrep  '^[0-9]{8}' | sed 's/.\{2\}$//' | sort | uniq"
+
 # Step1: create year directory
-for dir in $(yr-mn-in-photo-dir)
+for dir in $(${cmd})
 do
    month=$(echo ${dir: 4:2})
    year=$(echo ${dir: 0:4})
    dir=${year}/${month_converter[${month}]}
+
+   [[ ${dest_dir} != "" ]] \
+      && dir=${dest_dir}/${dir}
+
    [[ ! -d ${dir}  ]] \
       && mkdir -p ${dir} \
       && number_of_dirs_created=$(($number_of_dirs_created+1))
@@ -48,7 +87,17 @@ do
    year=$(echo ${file: 0:4})
    month=$(echo ${file: 4:2})
    month_exp=${month_converter[${month}]}
-   mv ${file} ${year}/${month_exp}
+
+   dir=${year}/${month_exp}
+   [[ ${dest_dir} != "" ]] \
+      && dir=${dest_dir}/${dir}
+
+   mv -n ${file} ${dir}/${file}
+
+   [[ $? -ne 0 ]] \
+      && echo "${file} existing, not overwriting" \
+      && continue
+
    number_of_files_moved=$(($number_of_files_moved+1))
 done
 
---
+++ vi_changed_files.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import commands
+import os
+from subprocess import call
+
+EDITOR = os.environ.get('EDITOR','vim') #that easy!
+
+modified_op = commands.getstatusoutput(
+        'git status --u=no | grep modified: | awk \'{print $2}\'')
+
+new_files_op = commands.getstatusoutput(
+        'git status --u=no | grep \"new file:\" | awk \'{print $3}\'')
+
+list_of_files=[]
+if modified_op[0] == 0:
+    list_of_files.extend(modified_op[1].split('\n'))
+
+if new_files_op[0] == 0:
+    list_of_files.extend(new_files_op[1].split('\n'))
+
+#Display
+count = 0
+
+print ''
+for file in list_of_files:
+    print '[' + str(count) + '] ' + file
+    count = count + 1
+
+
+while True:
+    print ''
+    selection = input('Select the file: ')
+
+    print ''
+    print 'Opening ' + list_of_files[selection]
+
+    print ''
+    confirm = raw_input("Continue y/n : ")
+
+    if confirm.lower() == 'y' or confirm.lower() == 'yes':
+        call ([EDITOR, list_of_files[selection]])
+        break
+
+    continue
