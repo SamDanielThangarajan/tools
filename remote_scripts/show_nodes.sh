2b6490f896ea59dcb1945f55c8e24254c1c9b65b
Sam Daniel <samdaniel.thangarajan@gmail.com>
2018-07-14 12:28:57 +0200

    shownodes ref

--- remote_scripts/show_nodes.sh
+++ remote_scripts/show_nodes.sh
@@ -4,40 +4,29 @@
 tput smcup
 
 function restore_screen() {
-   rm -rf .sn.tmp
+   rm -rf .sn.tmp 2>/dev/null
    tput rmcup
    exit 0
 }
 trap restore_screen INT
 
 function show_nodes() {
-   #Show nodes
-   # newline is replaced by |.
-   echo "ALIAS NODE USER" > $1
+   echo "ALIAS NODE USER"  > $1
    echo "***** ***** *****" >> $1
    while read line
    do
-      [[ $line =~ ^# ]] && continue
-      token_count=$(echo $line | wc -w)
-      [[ $token_count -ne 4 ]] && continue
-
-      alias=$(echo $line | awk '{print $1}')
-      node=$(echo $line | awk '{print $2}')
-      user=$(echo $line | awk '{print $3}')
-
+      [[ $(echo $line | wc -w) -ne 4 ]] && continue
       echo -e "" >> $1
-      echo -e "$alias $node $user" >> $1
+      echo $line | awk '{print $1 " " $2 " " $3}' >> $1
    done<~/nodes.cfg
 }
 
-rm -rf .sn.tmp
 show_nodes .sn.tmp
-
 cat .sn.tmp | column -t
 
 # Escape
 while true
 do
    read -n 1 var
-   [[ $var = 'q' ]] && restore_screen && exit 0
+   [[ $var = 'q' ]] && restore_screen
 done
